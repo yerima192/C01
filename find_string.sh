d38ba5cabb296e159aa82fae20dd64534e9a4d75
Abdoul Razack Samira <yerimas@iMac-de-Abdoul.local>
2025-07-07 02:23:53 +0100

    b

--- find_string.sh
+++ find_string.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
+# un script qui recherche une chaîne de caractères spécifique dans un fichier
+read fichier 
 
-# Demander à l'utilisateur d'entrer le nom du fichier
-echo -n "Entrez le nom du fichier :"
-read fichier
-
-# Demander à l'utilisateur d'entrer la chaîne à rechercher
-echo "Entrez la chaîne à rechercher :"
 read chaine
 
-# Rechercher la chaîne dans le fichier
 if grep -q "$chaine" "$fichier"; then
     echo "La chaîne '$chaine' a été trouvée dans $fichier."
-else 
+else
     echo "La chaîne '$chaine' n'a pas été trouvée dans $fichier."
 fi
