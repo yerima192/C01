1ef269b9d97f0f0bfa8b55b1c1a731fda46c3ac1
Abdoul Razack Samira <yerimas@iMac-de-Abdoul.local>
2025-07-09 12:14:03 +0100

    bb

---
+++ find_string.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+read fichier
+read chaine
+
+if grep "$chaine" "$fichier"; then 
+echo " la chaine '$chaine' a été trouvée dans le fichier ."
+else 
+echo "la chaine '$chaine' n a pas été trouvée dans le fichier ."
+fi
+
+
