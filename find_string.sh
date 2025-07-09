bb28051c5dc0881d1637bde2ec344007a449694a
yerima192 <razacksamira8@gmail.com>
2025-07-09 12:21:54 +0100

    bb

--- find_string.sh
+++ find_string.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 read fichier
 read chaine
-
 if grep "$chaine" "$fichier"; then 
 echo " la chaine '$chaine' a été trouvée dans le fichier ."
 else 
 echo "la chaine '$chaine' n a pas été trouvée dans le fichier ."
 fi
-
-
