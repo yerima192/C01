0787f78f694c7f2eab4d1320d10d13abca5789bc
yerima192 <razacksamira8@gmail.com>
2025-07-10 00:44:37 +0100

    bb

--- cli_arguments.sh
+++ cli_arguments.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-read -a args
+read -a args         # Lit une ligne et stocke chaque mot dans le tableau 'args'
 i=1
 for arg in "${args[@]}"; do
-echo "Argument $i : $arg" 
-i=$((i + 1))
-done 
+  echo "Argument $i : $arg"  # Affiche chaque élément avec son numéro
+  i=$((i + 1))               # Incrémente le compteur
+done
