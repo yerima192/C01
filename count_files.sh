77404f6efdebb646e5296b118e9e19bc2c556d79
Abdoul Razack Samira <yerimas@iMac-de-Abdoul.local>
2025-07-09 11:04:34 +0100

    bb

--- count_files.sh
+++ count_files.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
-
-echo -n "Entrez le nom du dossier :"
+echo -n "le nom du dossier:"
 read dossier
-
-if [  "$dossier" ]; then
-  count=$(ls "$dossier" | wc -l)
-  echo "Le dossier '$dossier' contient $count fichier(s)."
-else
-  echo "Erreur : le dossier '$dossier' n'existe pas."
-fi
+count=$(ls $dossier | wc -l)
+counts=$(echo $count)
+echo "Le dossier $dossier contient $counts fichier(s)."
