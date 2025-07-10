3f41a048c976cb6944563966f654c4f7d0297c28
yerima192 <razacksamira8@gmail.com>
2025-07-10 01:20:05 +0100

    commit

--- cli_arguments.sh
+++ cli_arguments.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-
+read -a args         # Lit une ligne et stocke chaque mot dans le tableau 'args'
 i=1
-while [ $# -gt 0 ]; do
-  echo "Argument $i: $1"
-  shift
-  ((i++))
+for arg in "${args[@]}"; do
+  echo "Argument $i: $arg"  # Affiche chaque élément avec son numéro
+  ((i++))               # Incrémente le compteur
 done
