844c0960ba7838ebfc12d663fac81099922952f2
Abassa Soumana <124785233+deloabass@users.noreply.github.com>
2025-07-11 02:17:57 +0100

    Update simple_calculator.sh

--- simple_calculator.sh
+++ simple_calculator.sh
@@ -1,22 +1,13 @@
 #!/bin/bash
-
-# L'utilisateur entre tout sur une seule ligne
-read -p "Entrez l'opération (ex : 5 3 +) : " a b op
-
-# Calcul selon l'opérateur
+read a b op
 if [ "$op" = "+" ]; then
-  echo "Résultat : $((a + b))"
+  echo $((a + b))
 elif [ "$op" = "-" ]; then
-  echo "Résultat : $((a - b))"
+  echo $((a - b))
 elif [ "$op" = "*" ]; then
-  echo "Résultat : $((a * b))"
+  echo $((a * b))
 elif [ "$op" = "/" ]; then
-  if [ "$b" -eq 0 ]; then
-    echo "❌ Division par zéro impossible"
-  else
-    echo "Résultat : $((a / b))"
-  fi
+  echo $((a / b))
 else
-  echo "❌ Opérateur invalide. Utilisez +, -, *, /"
+  echo "Opérateur invalide"
 fi
- 
