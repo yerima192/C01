e570d810c6cb9cda90d3cfd3a17ec97fa6ed308c
yerima192 <razacksamira8@gmail.com>
2025-07-17 01:32:51 +0100

    b

--- simple_calculator.sh
+++ simple_calculator.sh
@@ -8,21 +8,12 @@ elif [ "$op" = "-" ]; then
 echo "Results $((a - b))"
  elif [ "$op" = "*" ]; then
  echo " Results $((a * b))"
-
-
-
  elif [ "$op" = "/" ]; then
-
-  
- if [ "$b" -eq 0]; then         
-
+ if [ "$b" -eq 0 ]; then         
  echo "Imposible de diviser par zéro"
-
  else 
-
 echo " Results $((a / b))"
 fi
-
 else
 echo "Operation invalide"
 fi
