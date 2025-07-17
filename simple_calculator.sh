a2fcadfb2d767982f7de6aaf25e45313a9ad173e
yerima192 <razacksamira8@gmail.com>
2025-07-17 01:43:36 +0100

    b

--- simple_calculator.sh
+++ simple_calculator.sh
@@ -1,33 +1,25 @@
 #!/bin/bash
 
-read a b op
+# Lecture de 3 valeurs sur une seule ligne
+read -p "L'utilisation de deux nombres et un operateur : " a b op
 
+# Vérifie et effectue l'opération
 if [ "$op" = "+" ]; then
-
-echo "Results : $((a + b))"
+  echo "Résultat : $((a + b))"
 
 elif [ "$op" = "-" ]; then
+  echo "Résultat : $((a - b))"
 
-echo "Results : $((a - b))"
+elif [ "$op" = "*" ]; then
+  echo "Résultat : $((a * b))"
 
- elif [ "$op" = "*" ]; then
-
- echo "Results : $((a * b))"
-
- elif [ "$op" = "/" ]; then
-
- if [ "$b" -eq 0 ]; then      
-
- echo "Imposible de diviser par zéro"
-
-else 
-
-echo "Results : $((a / b))"
-
-fi
+elif [ "$op" = "/" ]; then
+  if [ "$b" -eq 0 ]; then
+    echo "❌ Impossible de diviser par zéro"
+  else
+    echo "Résultat : $((a / b))"
+  fi
 
 else
-
-echo "Operation invalide.  Utilisez +, -, * ou /"
-
+  echo "❌ Opération invalide. Utilisez +, -, * ou /"
 fi
