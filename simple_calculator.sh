7d07ce46a0cb59b82c6bdee4dca00455bbf85dbc
yerima192 <razacksamira8@gmail.com>
2025-07-17 01:36:04 +0100

    b

--- simple_calculator.sh
+++ simple_calculator.sh
@@ -3,17 +3,31 @@
 read -p " L'utilisation de deux nombres et un operateur : " a b op
 
 if [ "$op" = "+" ]; then
-echo "Results $((a + b))"
+
+echo "Results : $((a + b))"
+
 elif [ "$op" = "-" ]; then
-echo "Results $((a - b))"
+
+echo "Results : $((a - b))"
+
  elif [ "$op" = "*" ]; then
- echo " Results $((a * b))"
+
+ echo " Results : $((a * b))"
+
  elif [ "$op" = "/" ]; then
- if [ "$b" -eq 0 ]; then         
+
+ if [ "$b" -eq 0 ]; then      
+
  echo "Imposible de diviser par zéro"
- else 
-echo " Results $((a / b))"
+
+else 
+
+echo " Results : $((a / b))"
+
 fi
+
 else
+
 echo "Operation invalide"
+
 fi
